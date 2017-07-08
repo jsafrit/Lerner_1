2a0a3b04bbfff9ed22c8c1c1888c2965675a0600
John Safrit <jsafrit@users.noreply.github.com>
2017-07-08 15:13:19 -0400

    Added Lerner regex solution

--- ex4.py
+++ ex4.py
@@ -1,3 +1,6 @@
+import re
+
+
 def line_to_dict(filename):
     result_list = []
     with open(filename, 'r') as f:
@@ -18,7 +21,39 @@ def line_to_dict(filename):
     return result_list
 
 
+def re_line_to_dict(line):
+    regexp = '''
+        ((?:\d{1,3}\.){3}\d{1,3})       # IP addresses contain four numbers (each with 1-3 digits)
+        .*                              # Junk between IP address and timestamp
+        \[([^\]]+)\]                    # Timestamp, defined to be anything between [ and ]
+        .*                              # Junk between timestamp and request
+        "(GET[^"]+)"                    # Request, starting with GET
+        '''
+    m = re.search(regexp, line, re.X)
+
+    if m:
+        ip_address = m.group(1)
+        timestamp = m.group(2)
+        request = m.group(3)
+
+    else:
+        ip_address = 'No IP address found'
+        timestamp = 'No timestamp found'
+        request = 'No request found'
+
+    output = {'ip_address': ip_address,
+              'timestamp': timestamp,
+              'request': request}
+    return output
+
+
+def log_to_dict(filename):
+    return (re_line_to_dict(line)
+            for line in open(filename))
+
+
 if __name__ == '__main__':
-    result = line_to_dict('mini-access-log.txt')
+    # result = line_to_dict('mini-access-log.txt')
+    result = log_to_dict('mini-access-log.txt')
     for entry in result:
         print(entry)
